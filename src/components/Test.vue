789881342946d7e60c4e607d3ec9cfcfe4a1342e
ma <poet0@vip.qq.com>
2018-10-31 17:32:23 +0800

    test1 slot test2 .sync 和 :update

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot name="p"></slot>
+    <slot></slot>
+    <slot name="span"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+/*
+* author: mamingyang@baofeng.com
+* date: 2018/10/31
+*/
+
+<template>
+  <div>
+    <Test>
+      <p slot="p">abc</p>
+      <span slot="span">def</span>
+      <h1 @click="show=!show">abcabc</h1>
+    </Test>
+    <Test2 :value.sync="doc.title"></Test2>
+    <el-dialog :visible.sync="show">
+      <div slot="title">
+        我是标题
+      </div>
+      abcabc
+      <div slot="footer">
+        <el-button @click="show=!show">关闭</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    Test: () => import('../components/Test'),
+    Test2: () => import('../components/Test2'),
+  },
+  data() {
+    return {
+      show: false,
+      doc: {
+        title: '',
+      },
+    };
+  },
+  methods: {},
+  created() {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
