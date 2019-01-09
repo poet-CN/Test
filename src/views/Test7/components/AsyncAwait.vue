4053760dbc4fe66fa9eeab856600cb20e118098d
ma <poet0@vip.qq.com>
2019-01-09 10:35:22 +0800

    add async await

--- src/views/Test7/components/AsyncAwait.vue
+++ src/views/Test7/components/AsyncAwait.vue
@@ -15,28 +15,28 @@
             console.log( 'async1 start' )
             await async2()
             console.log( 'async1 end' )
-        }
+          }
 
-        async function async2() {
-            console.log( 'async2' )
-        }
+          async function async2() {
+              console.log( 'async2' )
+          }
 
-        console.log( 'script start' )
+          console.log( 'script start' )
 
-        setTimeout( function () {
-            console.log( 'setTimeout' )
-        }, 0 )
+          setTimeout( function () {
+              console.log( 'setTimeout' )
+          }, 0 )
 
-        async1();
+          async1();
 
-        new Promise( function ( resolve ) {
-            console.log( 'promise1' )
-            resolve();
-        } ).then( function () {
-            console.log( 'promise2' )
-        } )
+          new Promise( function ( resolve ) {
+              console.log( 'promise1' )
+              resolve();
+          } ).then( function () {
+              console.log( 'promise2' )
+          } )
 
-        console.log( 'script end' )
+          console.log( 'script end' )
     </pre>
     </el-card>
   </div>
